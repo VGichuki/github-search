34a756c47d6b4146e9eb6c51d6295a06e0062009
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-05-23 19:01:59 +0300

    add new text to the app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { UserComponent } from './git-form/user.component';
+import { RepoComponent } from './git-results/repo.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path: 'home', component: HomeComponent },
+  {path: 'contact', component: ContactComponent},
+  {path: 'gitForm', component: UserComponent},
+  {path: 'gitResults', component: RepoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
