f2f58401b5d1b187f3efc1ae39afb0b277808d02
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-05-23 20:48:50 +0300

    add new text to service

--- src/app/search-github.service.ts
+++ src/app/search-github.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { User } from './user'
+import { Repo } from './repo'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchGithubService {
 
-  constructor() { }
+  user: User | undefined;
+  repo: Repo | undefined;
+  repoData = [];
+  
+  constructor(private http: HttpClient) { 
+    this.user = new User("",0,"","",new Date)
+  }
 }
