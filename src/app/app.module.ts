904df77c52282ec4840ad130c6296a4bcb0aa59b
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-05-23 19:00:59 +0300

    add routing to the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RepoComponent } from './repo/repo.component';
-import { UserComponent } from './user/user.component';
+import { RepoComponent } from './git-results/repo.component';
+import { UserComponent } from './git-form/user.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RepoComponent,
-    UserComponent
+    UserComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
